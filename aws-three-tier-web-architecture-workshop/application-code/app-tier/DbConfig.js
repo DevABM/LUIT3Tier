ac629159e062b37490442c30c5569891334619e8
Amudha Balamurugan <100310286+DevABM@users.noreply.github.com>
2024-04-16 00:45:46 -0400

    3 tier updated

---
+++ aws-three-tier-web-architecture-workshop/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'datatier-instance-1.cgfvz9k8d1ca.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'adminadmin',
+    DB_DATABASE : 'webappdb'
+});
